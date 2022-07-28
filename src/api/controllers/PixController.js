0be2fd10be9189e0ccdc0529ebc4b9e8a8ec7fc0
Luan Henrique <luan.henrique@venhapranuvem.com>
2022-07-28 04:35:01 -0300

    corrigindo retorno das requisições a api

--- src/api/controllers/PixController.js
+++ src/api/controllers/PixController.js
@@ -1,15 +1,14 @@
-const mercadopago = require('mercadopago');
 const payment_data = require('../../config/pix-mercadopago');
 const HTTP = require('../../constants/http');
 const PixService = require('../services/PixService');
-mercadopago.configurations.setAccessToken('');
+
 
 class PixController {
     async createPayment(req, res, next) {
         try{
             const response = await PixService.createCharge();
             console.log(response)
-            return res.json(response)
+            return res.status(HTTP.OK).json(response)
         }catch(e){
             return next(e)
         }
@@ -20,9 +19,7 @@ class PixController {
             const {id} = req.params
 
             const response = await PixService.checkPayment(id);
-            return res.status(HTTP.OK).json({
-                status: response
-            })
+            return res.status(HTTP.OK).json(response)
         }catch(e){
             return next(e)
         }
